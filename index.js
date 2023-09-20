71933973a55e960de0338280c9d9fd8725ed42bc
Alan123alan <alancastellanos2@hotmail.com>
2023-09-19 21:09:25 -0600

    Replaced map with a function to set attributes

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
-//TO DO: Find a way to pass attributes with less overhead than by passing maps
+//TO DO: use rest parameters to accept any amount of children for a created element
 window.addEventListener("load", render);
 
 function render(){
     const root = document.getElementById("root");
     console.log(root);
-    const newPAttrs = new Map();
-    newPAttrs.set("id", "my-p")
-    const newP = p("some text", newPAttrs);
-    const newInputAttrs = new Map();
-    newInputAttrs.set("name", "first-input")
-    newInputAttrs.set("type", "email")
-    const newInput = input(newInputAttrs);
+    // const newPAttrs = new Map();
+    // newPAttrs.set("id", "my-p")
+    const newP = p("some text");
+    console.log(newP.attr("id", "my-p"));
+    // const newInputAttrs = new Map();
+    // newInputAttrs.set("name", "first-input")
+    // newInputAttrs.set("type", "email")
+    const newInput = input();
+    newInput.attr("name", "my-input")
+    newInput.attr("type", "password")
     root.appendChild(newP);
     root.appendChild(newInput);
 }
 
-function tag(name){
-    return document.createElement(name);
+function element(name){
+    let element = document.createElement(name);
+    element.attr = function(name, value){
+        this.setAttribute(name, value);
+        return this;
+    }
+    return element;
 }
 
-function p(text, attributes){
-    const p = tag("p");
+function p(text){
+    const p = element("p");
     if(text){
         p.appendChild(document.createTextNode(text));
         
     }
-    for(const [key, value] of attributes){
-        p.setAttribute(key, value)
-    }
     return p;
 }
 
-function input(attributes){
-    console.log(attributes);
-    const input = tag("input");
-    // function attrs(){
-    //     this.input.setAttribute("type", "email")
-    // }
-    for(const [key, value] of attributes){
-        input.setAttribute(key, value)
-    }
+function input(){
+    const input = element("input");
     return input;
 }
